5b193bb557ca90f9101213801d2e5ef309875333
mathew <chavrymsthew@gmail.com>
2023-09-19 13:50:18 -0400

    bug

--- interfaceBasicDoNow/ViewController.swift
+++ interfaceBasicDoNow/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
+    
+    
+    @IBOutlet weak var label: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    
+    
+    @IBAction func Greet(_ sender: UIButton) {
+        label.text = textField.text
+    }
 }
 
